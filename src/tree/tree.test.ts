dbda65429980ad03b6f8178f98d973b8a88302b7
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-06-18 21:21:04 +0200

    💡 remove comments

--- src/tree/tree.test.ts
+++ src/tree/tree.test.ts
@@ -163,46 +163,4 @@ describe('Hierarchical Data Structure Tree', () => {
 
     expect(node.getValue()).toBe('Borb');
   });
-
-  // it("should throw error if position doesn't exist", () => {
-  //   const { appendNewNodeToPosition } = createHierarchicalTree('Hello World');
-
-  //   expect(() => {
-  //     appendNewNodeToPosition('Hello World 2', 5);
-  //   }).toThrowError(/couldn't add new node to position 5./i);
-  // });
-
-  // it('should find node by position', () => {
-  //   const { appendNewNodeToPosition, findNodeByPosition } =
-  //     createHierarchicalTree('Hello World 0');
-  //   appendNewNodeToPosition('Hello World 1', 0);
-  //   appendNewNodeToPosition('Hello World 2', 0);
-  //   appendNewNodeToPosition('Hello World 3', 0);
-
-  //   expect(findNodeByPosition(2).getValue()).toBe('Hello World 2');
-  // });
-
-  // it('should add several nodes as a tree', () => {
-  //   const { appendNewNodeToPosition, getSize } =
-  //     createHierarchicalTree('Hello World 1');
-
-  //   appendNewNodeToPosition('Hello World 2', 0);
-  //   appendNewNodeToPosition('Hello World 3', 0);
-  //   appendNewNodeToPosition('Hello World 4', 2);
-  //   appendNewNodeToPosition('Hello World 5', 1);
-  //   appendNewNodeToPosition('Hello World 6', 4);
-  //   appendNewNodeToPosition('Hello World 7', 3);
-
-  //   expect(getSize()).toBe(7);
-  // });
-
-  // it('should find Node by ID', () => {
-  //   const { appendNewNodeToPosition, tree, findNodeById } = createHierarchicalTree('Hello World');
-  //   const { root: rootNode, size } = tree;
-  //   const newNode = appendNewNodeToPosition(rootNode, 'Hello World 2');
-
-  //   const node = findNodeById(rootNode.id);
-
-  //   expect(node).toBe(rootNode);
-  // });
 });
--- src/tree/tree.ts
+++ src/tree/tree.ts
@@ -3,21 +3,6 @@ import { nanoid } from 'nanoid';
 import { createNode } from './treeNode';
 import type { TreeOptions, Tree, Node, ID, BreadthFirstSearch } from './types';
 
-// const updateNodePositions = <T>(Node: Node<T>, appendedNode: Node<T>) => {
-//   if (
-//     Node.getPosition() >= appendedNode.getPosition() &&
-//     Node.getId() !== appendedNode.getId()
-//   ) {
-//     Node.updatePositionBy(1);
-//   } else {
-//     for (const child of Node.getChildren()) {
-//       if (child.getPosition() >= appendedNode.getPosition()) {
-//         updateNodePositions(child, appendedNode);
-//       }
-//     }
-//   }
-// };
-
 export function createHierarchicalTree<T>(
   value: T,
   options?: TreeOptions
@@ -71,7 +56,6 @@ export function createHierarchicalTree<T>(
     while (queue.length > 0) {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       const currentNode = queue.shift()!;
-      // console.log({ current: currentNode.getId(), nodeId });
       if (currentNode.getId() === nodeId) {
         result = currentNode;
         break;
@@ -106,103 +90,6 @@ export function createHierarchicalTree<T>(
     return result;
   };
 
-  // const appendNewNodeToPosition = (value: T, position: number) => {
-  //   let nextPosition = position + 1;
-
-  //   const traverse = (Node: Node<T>): Node<T> | null => {
-  //     let appended: Node<T>;
-  //     if (Node.getPosition() === position) {
-  //       const { append, getLastChild } = Node;
-  //       const lastChild = getLastChild();
-
-  //       if (lastChild) {
-  //         nextPosition = lastChild.getPosition() + 1;
-  //       }
-
-  //       const createdNode = createNode(
-  //         value,
-  //         setupOptions.createId,
-  //         nextPosition
-  //       );
-
-  //       appended = append(createdNode);
-  //       tree.size++;
-  //       return appended;
-  //     }
-  //     if (Node.getChildren().length > 0) {
-  //       for (const child of Node.getChildren()) {
-  //         const traversed = traverse(child);
-  //         if (traversed) {
-  //           return traversed;
-  //         }
-  //       }
-  //     }
-  //     return null;
-  //   };
-
-  //   const appendedNode = traverse(getRootNode());
-
-  //   if (appendedNode) {
-  //     updateNodePositions(getRootNode(), appendedNode);
-  //     return appendedNode;
-  //   }
-
-  //   throw new Error(`Couldn't add new node to Position ${position}.`);
-  // };
-
-  // const findNodeByPosition = (
-  //   position: number,
-  //   startingNode?: Node<T>
-  // ): Node<T> => {
-  //   const node = startingNode ?? getRootNode();
-
-  //   if (node.getPosition() === position) {
-  //     return node;
-  //   }
-
-  //   if (node.getChildren().length === 0) {
-  //     throw new Error(`Couldn't find node at position ${position}.`);
-  //   }
-
-  //   let pointer = node.getChildren().length - 1;
-
-  //   while (pointer >= 0) {
-  //     const child = node.getChildren()[pointer];
-  //     if (child.getPosition() <= position) {
-  //       const found = findNodeByPosition(position, child);
-  //       if (found) {
-  //         return found;
-  //       }
-  //       break;
-  //     }
-  //     pointer--;
-  //   }
-
-  //   throw new Error(`Couldn't find node at position ${position}.`);
-  // };
-
-  // const findNodeById: FindNodeById = (nodeId) => {
-  //   const node = tree.root;
-
-  //   if (node.id === nodeId) {
-  //     return node;
-  //   }
-
-  //   const traverse = <T>(node: Node<T>): Node<T> | null => {
-  //     if (node.children) {
-  //       for (const child of node.children) {
-  //         if (child.id === nodeId) {
-  //           return child;
-  //         }
-  //         traverse(child);
-  //       }
-  //     }
-  //     return null;
-  //   };
-
-  //   return traverse(node);
-  // };
-
   return {
     getSize,
     getTreeId,
@@ -212,13 +99,3 @@ export function createHierarchicalTree<T>(
     breadthFirstSearch,
   };
 }
-
-// function breadthFirstSearch<T>(
-//   Node: Node<T>,
-//   callback: (Node: Node<T>) => void
-// ) {
-//   callback(Node);
-//   for (const child of Node.getChildren()) {
-//     breadthFirstSearch(child, callback);
-//   }
-// }
